4addf2cef15f0f10a9d1a05273a3c4e5a21ea6b0
IsaiahAltDelete <isaiahsouf@charter.net>
2025-05-08 10:58:04 -0400

    Updated all repositories

---
+++ heroes/data-skills.js
@@ -0,0 +1,25 @@
+// data-skills.js
+
+const skills = [
+    { name: "Acrobatics (Dex)", icon: "sports_gymnastics" },
+    { name: "Stealth (Dex)", icon: "visibility_off" },
+    { name: "Sleight of Hand (Dex)", icon: "magic_button" },
+    { name: "Driving/Piloting (Dex)", icon: "flight_takeoff" },
+    { name: "Athletics (Str)", icon: "directions_run" },
+    { name: "Investigation (Int)", icon: "search" },
+    { name: "Technology (Int)", icon: "computer" },
+    { name: "History (Int)", icon: "history_edu" },
+    { name: "Mechanics/Engineering (Int)", icon: "build" },
+    { name: "Science (Bio/Chem/Phys) (Int)", icon: "science" },
+    { name: "Arcana (Int)", icon: "auto_stories" },
+    { name: "Insight (Wis)", icon: "psychology_alt" },
+    { name: "Perception (Wis)", icon: "visibility" },
+    { name: "Medicine (Wis)", icon: "medical_services" },
+    { name: "Survival (Wis)", icon: "forest" },
+    { name: "Streetwise (Wis)", icon: "location_city" },
+    { name: "Deception (Cha)", icon: "theater_comedy" },
+    { name: "Intimidation (Cha)", icon: "sports_kabaddi" },
+    { name: "Performance (Cha)", icon: "music_note" },
+    { name: "Persuasion (Cha)", icon: "record_voice_over" },
+    { name: "Leadership (Cha)", icon: "groups" }
+];
